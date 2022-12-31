7735803b74b439759bc30e5be0f303f6cb713327
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-01-01 01:57:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,8 +140,7 @@ if uploaded_file is not None:
  age = st.sidebar.slider("What is the age of the person?",0.0,150.0,50.0)
  smoker = st.sidebar.selectbox('Is the Person a Smoker',('Yes', 'No'))
 
-@st.cache
-def additional_stats():
+
  if age <= 5.0 and sex == 'Male':
   confidence = confidence_score * ((165+age)/100)
  if age > 5.0 and age <= 25.0 and sex == 'Male':
@@ -168,37 +167,36 @@ def additional_stats():
  if smoker == 'No':
   confidence2 = confidence
  #https://pubmed.ncbi.nlm.nih.gov/8246285/#:~:text=However%2C%20among%20participants%20aged%2060,CI%20%3D%200.97%2D11.9).
- return confidence, confidence2, confidence_score
-
-@st.cache
-def final_predicts():
- if confidence_score >= 0.85:
-
-  if index == 0:
-   st.write('You identify as', sex,'and your age is', age,'years')
-   st.write('The Predicted Class is:', class_name)
-   st.write('Probability Percentage:', confidence_score*100, '%')
-   st.write("You may have a benign condition, or are free of ALL. Get it checked once during your regular body check")
-   st.write("Benign tumors are those that stay in their primary location without invading other sites of the body. They do not spread to local structures or to distant parts of the body. Benign tumors tend to grow slowly and have distinct borders. Benign tumors are not usually problematic.")
 
 
-  else:
-   st.write('You identify as', sex,'and your age is', age,'years')
-   st.write('The Predicted Class is:', class_name)
-   st.write('The Original Probability Percentage:', confidence_score*100, '%')
-   st.write('Probability Percentage due to your age group:', confidence2 * 100, '%')
 
-  if index == 1:
-   st.write("This looks like a Malignant Pro-B variant of ALL. You need to get it checked before the cancer starts spreading")
-  elif index == 2:
-   st.write("This looks like a Malignant Pre-B variant of ALL. You need to get it checked ASAP before the condition metastisizes")
-  elif index == 3:
-   st.write("This looks like an early Malignant Pre-B variant of ALL. You need to get it checked as a priority before it becomes something serious")
-   st.write("Malignancy is a term for diseases in which abnormal cells divide without control and can invade nearby tissues. Malignant cells can also spread  to other parts of the body through the blood and lymph systems.")
+if confidence_score >= 0.85:
+
+ if index == 0:
+  st.write('You identify as', sex,'and your age is', age,'years')
+  st.write('The Predicted Class is:', class_name)
+  st.write('Probability Percentage:', confidence_score*100, '%')
+  st.write("You may have a benign condition, or are free of ALL. Get it checked once during your regular body check")
+  st.write("Benign tumors are those that stay in their primary location without invading other sites of the body. They do not spread to local structures or to distant parts of the body. Benign tumors tend to grow slowly and have distinct borders. Benign tumors are not usually problematic.")
 
 
  else:
-  st.write("You are free from ALL but don't forget to get a body checkup regularly")   
+  st.write('You identify as', sex,'and your age is', age,'years')
+  st.write('The Predicted Class is:', class_name)
+  st.write('The Original Probability Percentage:', confidence_score*100, '%')
+  st.write('Probability Percentage due to your age group:', confidence2 * 100, '%')
+
+ if index == 1:
+  st.write("This looks like a Malignant Pro-B variant of ALL. You need to get it checked before the cancer starts spreading")
+ elif index == 2:
+  st.write("This looks like a Malignant Pre-B variant of ALL. You need to get it checked ASAP before the condition metastisizes")
+ elif index == 3:
+  st.write("This looks like an early Malignant Pre-B variant of ALL. You need to get it checked as a priority before it becomes something serious")
+  st.write("Malignancy is a term for diseases in which abnormal cells divide without control and can invade nearby tissues. Malignant cells can also spread  to other parts of the body through the blood and lymph systems.")
+
+
+ else:
+ st.write("You are free from ALL but don't forget to get a body checkup regularly")   
 
 
 
