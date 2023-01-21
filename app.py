bed4065dd94eaf890700510465804a4f4ce2b35c
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-01-21 08:27:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
-from streamlit.components.v1 import iframe
 import os
 import pdfkit
-from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
-from datetime import date
 from PIL import Image
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
@@ -209,56 +206,6 @@ if confidence_score >= 0.85:
  else:
   st.write("You are free from ALL but don't forget to get a body checkup regularly")   
    
-
-
-st.set_page_config(layout="centered", page_icon="ðŸŽ“", page_title="Diploma Generator")
-st.title("ðŸŽ“ Diploma PDF Generator")
-
-st.write(
-    "This app shows you how you can use Streamlit to make a PDF generator app in just a few lines of code!"
-)
-
-left, right = st.columns(2)
-
-right.write("Here's the template we'll be using:")
-
-right.image("template.png", width=300)
-
-env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
-template = env.get_template("template.html")
-
-
-left.write("Fill in the data:")
-form = left.form("template_form")
-student = form.text_input("Student name")
-course = form.selectbox(
-    "Choose course",
-    ["Report Generation in Streamlit", "Advanced Cryptography"],
-    index=0,
-)
-grade = form.slider("Grade", 1, 100, 60)
-submit = form.form_submit_button("Generate PDF")
-
-if submit:
-    html = template.render(
-        student=student,
-        course=course,
-        grade=f"{grade}/100",
-        date=date.today().strftime("%B %d, %Y"),
-    )
-
-    pdf = pdfkit.from_string(html, False)
-    st.balloons()
-
-    right.success("ðŸŽ‰ Your diploma was generated!")
-    # st.write(html, unsafe_allow_html=True)
-    # st.write("")
-    right.download_button(
-        "â¬‡ï¸ Download PDF",
-        data=pdf,
-        file_name="diploma.pdf",
-        mime="application/octet-stream",
-    )
 
  
 
