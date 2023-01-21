e9f8a7b93c716b612b50d0506c44a08a8ab61ce1
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-01-21 19:16:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,55 +90,57 @@ class_names = open('model/labels.txt', 'r').readlines()
 # determined by the first position in the shape tuple, in this case 1.
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-st.header('Acute Lymphoblastic Leukemia Detector')
-st.subheader('The properties of the Slide pictures should be as follows:')
-st.subheader('1: Put a picture of a blood slide only')
-st.subheader('2: Results will be better if blood is taken from somewhere near bone marrow and lymph nodes')
-st.subheader('3: The slide should be at a 1000x magnification')             
-st.subheader('4: Make sure that the picture has minimal noise')            
+if login_button == True:
+ st.header('Welcome')
+ st.header('Acute Lymphoblastic Leukemia Detector')
+ st.subheader('The properties of the Slide pictures should be as follows:')
+ st.subheader('1: Put a picture of a blood slide only')
+ st.subheader('2: Results will be better if blood is taken from somewhere near bone marrow and lymph nodes')
+ st.subheader('3: The slide should be at a 1000x magnification')             
+ st.subheader('4: Make sure that the picture has minimal noise')            
              
              
              
              
 
-mode=st.sidebar.select_slider('Choose between using a jpg file or taking a picture using your camera', options=['JPG','CAMERA'])
+ mode=st.sidebar.select_slider('Choose between using a jpg file or taking a picture using your camera', options=['JPG','CAMERA'])
 
-if mode == 'CAMERA':
- uploaded_file = st.camera_input("Take a photo of the slide")
+ if mode == 'CAMERA':
+  uploaded_file = st.camera_input("Take a photo of the slide")
 
 
-if mode == 'JPG':
- uploaded_file = st.file_uploader("Choose an Image", type="jpg")
+ if mode == 'JPG':
+  uploaded_file = st.file_uploader("Choose an Image", type="jpg")
 
-image = Image.open(uploaded_file).convert('RGB')
+ image = Image.open(uploaded_file).convert('RGB')
 
 
 
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+ size = (224, 224)
+ image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
 #turn the image into a numpy array
-image_array = np.asarray(image)
+ image_array = np.asarray(image)
 
 # Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+ normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
 # Load the image into the array
-data[0] = normalized_image_array
+ data[0] = normalized_image_array
 
 # run the inference
 
-prediction = model.predict(data)
-index = np.argmax(prediction)
-class_name = class_names[index]
-confidence_score = prediction[0][index]
-confidence = confidence_score
-print('Class:', class_name, end='')
-print('Confidence score:', confidence_score)
+ prediction = model.predict(data)
+ index = np.argmax(prediction)
+ class_name = class_names[index]
+ confidence_score = prediction[0][index]
+ confidence = confidence_score
+ print('Class:', class_name, end='')
+ print('Confidence score:', confidence_score)
 
-def create_report():
+ def create_report():
     st.title("Medical Report")
 
     patient_name = st.text_input("Patient Name")
