465f753ac4d42e2f322e1f7d95a7a6edc65a4c56
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-01-21 18:14:14 +0530

    Add files via upload

---
+++ Wallet/wallet_connect/frontend/build/precache-manifest.eb1eaae15a885c6d5292a906c0df967e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab186f39ea0b82520f2631c402972e67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8a1c4b037cd3bdd72b2",
+    "url": "./static/js/2.d0d0ffc2.chunk.js"
+  },
+  {
+    "revision": "49c7851b7c64ecc9408709042dce2a90",
+    "url": "./static/js/2.d0d0ffc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee963620144eec23d17e",
+    "url": "./static/js/main.35445d24.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.35445d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675ebed85599bcec64c0",
+    "url": "./static/js/runtime-main.57a1a595.js"
+  }
+]);
